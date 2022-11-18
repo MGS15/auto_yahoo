99f79ffd49500f64dc79f0f09b2e783ecfbd0170
Soufiane Elkhamlichi <05soufiane@gmail.com>
2022-11-18 19:04:28 +0100

    Define some global variables

---
+++ src/init/globals.py
@@ -0,0 +1,16 @@
+import os.path
+
+# Colors
+Black = "\033[0;30m"        # Black
+White = "\033[0;37m"        # White
+Red = "\033[0;31m"          # Red
+Green = "\033[0;32m"        # Green
+Yellow = "\033[0;33m"       # Yellow
+Blue = "\033[0;34m"         # Blue
+BBlack = "\033[1;30m"       # Black
+BRed = "\033[1;31m"         # Red
+BGreen = "\033[1;32m"       # Green
+
+# PATHS
+ASSETS = "assets"
+DRIVER_PATH = ASSETS + os.path.sep + "geckodriver"
