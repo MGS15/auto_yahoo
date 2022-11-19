0294e26ce3ebd45bcb56d64cfd7140597808da85
Soufiane Elkhamlichi <05soufiane@gmail.com>
2022-11-19 18:09:16 +0100

    Account class with getters & setters

---
+++ src/modules/Account.py
@@ -0,0 +1,48 @@
+import init.globals as globals
+import re
+
+class Account:
+	def __init__(self):
+		self.email = ""
+		self.password = ""
+		self.proxyIp = ""
+		self.proxyPort = ""
+		self.proxyUser = ""
+		self.proxyPassword = ""
+
+	# Getters
+	def getEmail(self):
+		return self.email
+	def getPassword(self):
+		return self.password
+	def getProxyIp(self):
+		return self.proxyIp
+	def getProxyPort(self):
+		return self.proxyPort
+	def getProxyUser(self):
+		return self.proxyUser
+	def getProxyPassword(self):
+		return self.proxyPassword
+	
+	# Setters
+	def setEmail(self, email):
+		if re.fullmatch("[a-zA-Z0-9]{2,50}[\-|_]*[a-zA-Z0-9]{2,50}@yahoo{1}(\.[a-zA-Z]{2,4}){1,2}", email) == None:
+			raise Exception(globals.Red + "All emails should be valid!" + globals.White)
+		else:
+			self.email = email
+	def setPassword(self, password):
+		self.password = password
+	def setProxyIp(self, proxyIp):
+		if re.fullmatch("\d{1,3}\.\d{1,3}\.\d{1,3}", proxyIp) == None:
+			raise Exception(globals.Red + "Proxy must be valid!" + globals.White)
+		else:
+			self.proxyIp = proxyIp
+	def setProxyPort(self, port: int):
+		if port in range(0, 65535):
+			self.proxyPort = port
+		else:
+			raise Exception(globals.Red + "Proxy's port must be between 0 and 65535!" + globals.White)
+	def setProxyUser(self, username):
+		self.proxyUser = username
+	def setProxyPassword(self, password):
+		self.proxyPassword = password
