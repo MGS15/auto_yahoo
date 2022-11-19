9225528a17f94e65703022184e577db55ee1ce09
Soufiane Elkhamlichi <05soufiane@gmail.com>
2022-11-19 20:21:21 +0100

    InboxActions class

---
+++ src/modules/InboxActions.py
@@ -0,0 +1,31 @@
+import init.globals as globals
+
+class InboxActions:
+	def __init__(self):
+		self.reply = 90
+		self.archive = 90
+		self.star = 90
+	
+	# Getters
+	def getReply(self):
+		return self.reply
+	def getArchive(self):
+		return self.archive
+	def getStar(self):
+		return self.star
+	# Setters
+	def setReply(self, reply: int):
+		if reply in range(0, 100):
+			self.reply = reply
+		else:
+			raise Exception(globals.Red + "This action requires a value between 0 and 100!" + globals.White)
+	def setArchive(self, archive: int):
+		if archive in range(0, 100):
+			self.archive = archive
+		else:
+			raise Exception(globals.Red + "This action requires a value between 0 and 100!" + globals.White)
+	def setStar(self, star: int):
+		if star in range(0, 100):
+			self.star = star
+		else:
+			raise Exception(globals.Red + "This action requires a value between 0 and 100!" + globals.White)
