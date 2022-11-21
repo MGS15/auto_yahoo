1a58410138ea735e645b6fecbe31a7555cfd0b91
Soufiane Elkhamlichi <05soufiane@gmail.com>
2022-11-21 17:39:31 +0100

    init_config is done

--- src/init/init_config.py
+++ src/init/init_config.py
@@ -33,7 +33,16 @@ def getInputs():
 	config = Config.Config()
 	config.setEmailFrom(inputStrings("Enter email from: "))
 	config.setEmailSubject(inputStrings("Enter email subject: "))
-	config.setTimeout(int(inputInt("Enter timeout value: ", 0, 18000)))
-	config.setNumberOfThreads(int(inputInt("Enter number of threads: ", 0, 50)))
+	config.setTimeout(int(inputInt("Enter timeout value: ", -1, 180001)))
+	config.setNumberOfThreads(int(inputInt("Enter number of threads: ", -1, 51)))
+	print("Spam actions:")
+	config.spamActions.setRestoreInbox(int(inputInt("\tRestore inbox ratio / 100: ", -1, 101)))
+	config.spamActions.setMoveToInbox(int(inputInt("\tMove to inbox ratio / 100: ", -1, 101)))
+	config.spamActions.setNotSpam(int(inputInt("\tNot spam ratio / 100: ", -1, 101)))
+	print("Inbox actions:")
+	config.inboxActions.setArchive(int(inputInt("\tArchive / 100: ", -1, 101)))
+	config.inboxActions.setStar(int(inputInt("\tStar / 100: ", -1, 101)))
+	config.inboxActions.setReply(int(inputInt("\tReply / 100: ", -1, 101)))
+	config.inboxActions.setReplyMessage(input("Set custome message for reply (Leave empty to randomize): "))
 	return config
 	
