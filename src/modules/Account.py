aa94c958b54b566dfcd15523a247687f9a1bbd95
Soufiane Elkhamlichi <05soufiane@gmail.com>
2022-11-21 20:43:24 +0100

    Account class's elements protection

--- src/modules/Account.py
+++ src/modules/Account.py
@@ -26,14 +26,17 @@ class Account:
 	
 	# Setters
 	def setEmail(self, email):
-		if re.fullmatch("[a-zA-Z0-9]{2,50}[\-|_]*[a-zA-Z0-9]{2,50}@yahoo{1}(\.[a-zA-Z]{2,4}){1,2}", email) == None:
+		if re.fullmatch("[a-zA-Z0-9]{2,50}[\-|_|\.]*[a-zA-Z0-9]{2,50}@yahoo{1}(\.[a-zA-Z]{2,4}){1,2}", email) == None:
 			raise Exception(globals.Red + "All emails should be valid!" + globals.White)
 		else:
 			self.email = email
 	def setPassword(self, password):
-		self.password = password
+		if password == "" or password == None:
+			raise Exception(globals.Red + "Email password is empty!" + globals.White)
+		else:
+			self.password = password
 	def setProxyIp(self, proxyIp):
-		if re.fullmatch("\d{1,3}\.\d{1,3}\.\d{1,3}", proxyIp) == None:
+		if re.fullmatch('\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}', proxyIp) == None:
 			raise Exception(globals.Red + "Proxy must be valid!" + globals.White)
 		else:
 			self.proxyIp = proxyIp
@@ -43,6 +46,12 @@ class Account:
 		else:
 			raise Exception(globals.Red + "Proxy's port must be between 0 and 65535!" + globals.White)
 	def setProxyUser(self, username):
-		self.proxyUser = username
+		if username == "" or username ==None:
+			raise Exception(globals.Red + "Proxy user must be specified!" + globals.White)
+		else:
+			self.proxyUser = username
 	def setProxyPassword(self, password):
-		self.proxyPassword = password
+		if password == "" or password == None:
+			raise Exception(globals.Red + "Proxy password must be specified!" + globals.White)
+		else:
+			self.proxyPassword = password
