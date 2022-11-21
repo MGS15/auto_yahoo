53cb3b28efcab9c5c99c1524be3781143f2ac956
Soufiane Elkhamlichi <05soufiane@gmail.com>
2022-11-21 17:56:32 +0100

    Add generate_relpy_message function

--- src/helpers/randomize.py
+++ src/helpers/randomize.py
@@ -1,4 +1,8 @@
 import random
+import string
 
 def random_wait_time(min: int, max: int):
 	return random.randint(min, max)
+
+def generate_relpy_message():
+	random_reply = ''.join(random.choices(string.ascii_uppercase + string.ascii_lowercase, k=random.randint(100, 200)))
