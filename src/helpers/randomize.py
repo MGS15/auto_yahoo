6800d81a095d065e926149169f791f4820912d56
Soufiane Elkhamlichi <05soufiane@gmail.com>
2022-11-23 15:05:45 +0100

    update random_wait_time func

--- src/helpers/randomize.py
+++ src/helpers/randomize.py
@@ -1,8 +1,9 @@
 import random
 import string
+import time
 
 def random_wait_time(min: int, max: int):
-	return random.randint(min, max)
+	time.sleep(random.randint(min, max) / 10)
 
 def generate_relpy_message():
 	random_reply = ''.join(random.choices(string.ascii_uppercase + string.ascii_lowercase, k=random.randint(100, 200)))
