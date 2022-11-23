685f5765e3d3aea41fc56da3929ba64e8351a268
Soufiane Elkhamlichi <05soufiane@gmail.com>
2022-11-23 16:13:18 +0100

    Logger function

---
+++ src/helpers/logger.py
@@ -0,0 +1,25 @@
+from init import globals
+import datetime
+
+def get_time_stamp():
+	now = datetime.now()
+	timestamp = now.strftime("%Y/%m/%d - %H:%M:%S")
+	return timestamp
+
+def logger(level: int, email: str):
+	line = '[' + get_time_stamp() + '] '
+	if level == globals.EMAIL_ERROR:
+		line += 'EMAIL_ERROR\t\t: invalid email address: (' + email + ')'
+	elif level == globals.PASS_ERROR:
+		line += 'PASS_ERROR\t\t: invalid password: (' + email + ')'
+	elif level == globals.PROXY_ERROR:
+		line += 'PROXY_ERROR\t\t: invalid proxy address: (' + email + ')'
+	elif level == globals.CAPTCHA_ERROR:
+		line += 'CAPTCHA_ERROR\t\t: Captcha error accured!'
+	elif level == globals.BLOCKED_ACC_ERROR:
+		line += 'BLOCKED_ACC_ERROR\t\t: account is blocked: (' + email + ')'
+	else:
+		line += 'Unknown error: (' + email + ')'
+	logfile = open(globals.LOG_FILE, 'a+')
+	logfile.write(line)
+	logfile.close()
