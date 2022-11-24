ce2e8829831fab9d0656a66f3119c52b1fb58160
Soufiane Elkhamlichi <05soufiane@gmail.com>
2022-11-24 19:17:54 +0100

    Fix timeout

--- src/app/specifiers.py
+++ src/app/specifiers.py
@@ -3,8 +3,8 @@ from selenium import webdriver
 from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from modules.Config import Config
 from selenium.webdriver.common.by import By
+from helpers import logger
 
 def write_humanly(text: str, element: WebElement):
 	element.clear()
@@ -12,8 +12,8 @@ def write_humanly(text: str, element: WebElement):
 		randomize.random_wait_time(1, 3)
 		element.send_keys(char)
 
-def wait_for_element_by_id(id: str, config: Config, browser: webdriver.Chrome):
-	WebDriverWait(browser, config.getTimeOut()).until(
+def wait_for_element_by_id(id: str, timeout: int, browser: webdriver.Chrome):
+	WebDriverWait(browser, timeout).until(
 		EC.presence_of_element_located((By.ID, id))
 		)
 
