3b292dcc47e88b39e100fb1b3552e847b081fc05
Soufiane Elkhamlichi <05soufiane@gmail.com>
2022-11-24 13:46:41 +0100

    no changes

--- src/app/route.py
+++ src/app/route.py
@@ -7,9 +7,9 @@ def route():
 	try:
 		accounts = init_accounts.read_csv()
 		config = init_config.getInputs()
-		chwd = init_webdriver.init_webdriver(account=accounts[1])
+		chwd = init_webdriver.init_webdriver(account=accounts[0])
 		chwd.get("https://login.yahoo.com/")
-		account_handler.login(chwd, config, accounts[1])
+		account_handler.login(chwd, config, accounts[0])
 	except Exception as e:
 		print(globals.Red + "Error accured!" + globals.White)
 		traceback.print_exc()
