60480ab0cf5c43421ceb504c052fa929d695858c
Soufiane Elkhamlichi <05soufiane@gmail.com>
2022-11-24 19:18:07 +0100

    route function

--- src/app/route.py
+++ src/app/route.py
@@ -4,12 +4,8 @@ import time
 import traceback
 
 def route():
-	try:
-		accounts = init_accounts.read_csv()
-		config = init_config.getInputs()
-		chwd = init_webdriver.init_webdriver(account=accounts[0])
-		chwd.get("https://login.yahoo.com/")
-		account_handler.login(chwd, config, accounts[0])
-	except Exception as e:
-		print(globals.Red + "Error accured!" + globals.White)
-		traceback.print_exc()
+	accounts = init_accounts.read_csv()
+	config = init_config.getInputs()
+	chwd = init_webdriver.init_webdriver(account=accounts[0])
+	chwd.get("https://login.yahoo.com/")
+	account_handler.login(chwd, config, accounts[0])
