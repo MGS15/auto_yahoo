05eae19a97c5eeaa59c743547f32b05be481e263
Soufiane Elkhamlichi <05soufiane@gmail.com>
2022-11-25 21:10:32 +0100

    Unessessary packages

--- src/app/route.py
+++ src/app/route.py
@@ -1,7 +1,5 @@
 from init import globals as globals, init_webdriver as init_webdriver, init_config as init_config, init_accounts, init_config
 from app import account_handler
-import time
-import traceback
 
 def route():
 	accounts = init_accounts.read_csv()
