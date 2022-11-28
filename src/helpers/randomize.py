380c5b4deddba616d23ad94b781409381475cb83
Soufiane Elkhamlichi <05soufiane@gmail.com>
2022-11-28 15:21:17 +0100

    random_num_in_range

--- src/helpers/randomize.py
+++ src/helpers/randomize.py
@@ -5,6 +5,9 @@ import time
 def random_wait_time(min: int, max: int):
 	time.sleep(random.randint(min, max) / 10)
 
+def random_num_in_range(min: int, max: int):
+	return random.randint(min, max)
+
 def generate_relpy_message():
 	random_reply = ''.join(random.choices(string.ascii_uppercase + string.ascii_lowercase, k=random.randint(100, 200)))
 	return random_reply
