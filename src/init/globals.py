d99d26a6b165821dd1912c25b689dc516c5b26eb
Soufiane Elkhamlichi <05soufiane@gmail.com>
2022-12-03 13:13:19 +0100

    Update colors in globals

--- src/init/globals.py
+++ src/init/globals.py
@@ -3,15 +3,16 @@ from os.path import exists
 from os import mkdir
 
 # Colors
-Black = "\033[0;30m"        # Black
-White = "\033[0;37m"        # White
-Red = "\033[0;31m"          # Red
-Green = "\033[0;32m"        # Green
-Yellow = "\033[0;33m"       # Yellow
-Blue = "\033[0;34m"         # Blue
-BBlack = "\033[1;30m"       # Black
-BRed = "\033[1;31m"         # Red
-BGreen = "\033[1;32m"       # Green
+ForGround = '-ForegroundColor'
+Black = ForGround + ' ' + "Black"        # Black
+White = ForGround + ' ' + "White"        # White
+Red = ForGround + ' ' + "Red"          # Red
+Green = ForGround + ' ' + "Green"        # Green
+Yellow = ForGround + ' ' + "Yellow"       # Yellow
+Blue = ForGround + ' ' + "Blue"         # Blue
+BBlack = ForGround + ' ' + "DarkGray"       # Black
+BRed = ForGround + ' ' + "DarkRed"         # Red
+BGreen = ForGround + ' ' + "DarkGreen"       # Green
 
 # PATHS
 ASSETS = "assets"
